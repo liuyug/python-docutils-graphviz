a619a8d2286d02800a30ef8b442f8b9643cfd09a
LIU Yugang <liuyug@gmail.com>
2018-08-12 22:14:59 +0800

    version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as f:
 
 setup(
     name='python-docutils-graphviz',
-    version='1.0.1',
+    version='1.0.2',
     author='Yugang LIU',
     author_email='liuyug@gmail.com',
     url='https://github.com/liuyug/python-docutils-graphviz.git',
